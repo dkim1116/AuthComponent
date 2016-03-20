920fa05b24fc1c111bc7329dc24991c885ff1f6c
Daniel Kim <daniel.kim1116@gmail.com>
2016-03-19 19:09:34 -0700

    HOC work in progress

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Dummy functional component
+      <ul>
+        <li>Shouldnt render</li>
+        <li>When not</li>
+        <li>Authenticated</li>
+      </ul>
+    </div>
+  )
+}
