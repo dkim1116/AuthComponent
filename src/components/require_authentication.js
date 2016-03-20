3abf2ef463c2f4f23c456851aed7bc931f3531c9
Daniel Kim <daniel.kim1116@gmail.com>
2016-03-19 19:30:32 -0700

    HOC finished

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
       return (
         <ComposedComponent {...this.props} />
@@ -9,5 +26,9 @@ export default function(ComposedComponent) {
     }
   }
 
-  return Authentication;
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
